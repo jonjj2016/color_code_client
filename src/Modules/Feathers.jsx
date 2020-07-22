19276fa11949f06b467ee5ce8eeaa941a7ee16da
Jon Martirosyan <jonmartirosyan1988@gmail.com>
2020-07-22 09:14:34 -0700

    n

--- src/Modules/Feathers.jsx
+++ src/Modules/Feathers.jsx
@@ -2,8 +2,8 @@ import feathers from '@feathersjs/feathers';
 import axios from 'axios';
 import rest from '@feathersjs/rest-client';
 import auth from '@feathersjs/authentication-client';
-const HOST = 'http://localhost:3030';
-// const HOST = 'https://colorcode2020.herokuapp.com';
+// const HOST = 'http://localhost:3030';
+const HOST = 'https://colorcode2020.herokuapp.com';
 
 export const client = feathers()
   .configure(rest(HOST).axios(axios))
